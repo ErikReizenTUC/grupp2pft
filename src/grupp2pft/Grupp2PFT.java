a845bfd90b1fb83ef63806fe7582d8050d48d8bb
Erik Reizenstein <erik.reizenstein.mte21lin@tucsweden.se>
2021-03-02 13:48:47 +0100

    Initial commit

---
+++ src/grupp2pft/Grupp2PFT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupp2pft;
+
+/**
+ *
+ * @author Erik
+ */
+public class Grupp2PFT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
