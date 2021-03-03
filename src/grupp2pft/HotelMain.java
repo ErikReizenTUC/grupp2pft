3e80302b0f569244311f77bd6aec11e31522852c
Erik Reizenstein <erik.reizenstein.mte21lin@tucsweden.se>
2021-03-03 09:37:42 +0100

    Bytte namn på main-klassen

---
+++ src/grupp2pft/HotelMain.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ /*
+Gruppmedlemmar i grupp 2!
+Stefan Levstad
+Erik Reizenstein
+Pooja Bansal
+Marcus Ahlgren
+Ali Esmaeeli
+ */
+ 
+
+package grupp2pft;
+
+/**
+ *
+ * @author Grupp 2
+ */
+public class HotelMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
