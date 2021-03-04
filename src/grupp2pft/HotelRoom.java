77e2242ffe85f7e3b49203fae1c00d237e7d6654
Stefan Levstad <stefan.levstad.mte21lin@tucsweden.se>
2021-03-04 18:00:24 +0100

    Mer kommentarer i HotelRoom class.

--- src/grupp2pft/HotelRoom.java
+++ src/grupp2pft/HotelRoom.java
@@ -21,17 +21,19 @@ public class HotelRoom extends HotelMain{
         
         String newRoom;
         
+        //Creates an ArrayList of the rooms.
         ArrayList roomList = new ArrayList();
-  
         roomList.add("Room 1");
         roomList.add("Room 2");
         roomList.add("Room 3");
         roomList.add("Room 4");
         roomList.add("Room 5");
-        
+        //Prints out the list of avaiable rooms.
         System.out.println("Avaiable rooms: " + roomList);
         
-        System.out.println("Name the new room: ");
+        
+        //Lets the user create a new room and prints the list again.
+        System.out.println("Set number for the new room: ");
         newRoom = scan.nextLine();
         roomList.add(newRoom);
         System.out.println("Avaiable rooms: " + roomList);
