df6771a92a235b31194df4ad20ad1dd84015d6de
Ali Esmaeeli <ali.esmaeeli.mte21lin@tucsweden.se>
2021-03-04 17:25:05 +0100

    AliTest1

--- src/grupp2pft/HotelMain.java
+++ src/grupp2pft/HotelMain.java
@@ -28,9 +28,53 @@ public class HotelMain {
      */
     public static void main(String[] args) {
         
+             //This Scanner object can be used for diffrent cases
+        Scanner scan = new Scanner(System.in);
+       
+        //Create SwitchCase for particular person choice
+        System.out.println("Enter your choice: ");
+        System.out.println("1. Customer");
+        System.out.println("2. Receptionstaff");
+        System.out.println("3. Hotelstaff");
+                     
+        int choice = scan.nextInt();
+       
+        switch(choice){
+             //Customer can check room availability, booking room and prepayment option are available
+            case 1:
+                //Create SwitchCase for customer choice                               
+                System.out.println("Choose 1: check availability, 2: Checkin, 3: Checkout");
+                int value = scan.nextInt();
+               
+               
+                switch(value){
+                    case 1:
+                     //Show the list of available room by using hotelroom class
+                       
+                        //Create command here from hotelroom
+                        break;
+                    case 2:
+                        //copy command from case 1 to show available room
+                        System.out.println("Enter room number");
+                        int number = scan.nextInt();
+                        //change the status of room for entered room number
+                        System.out.println("You have booked the room. Congratulation!");
+                        break;
+                    case 3:
+                        System.out.println("Enter room number which you want to checkout");
+                        int t = scan.nextInt();
+                        //change the status of room for entered room number
+                        System.out.println("You have succesfully checked out");
+                        break;
+                    default:
+                        System.out.println("Try Again");
+                        break;
+                }
+                break;
     
     //calls the Rooms method in the HotelRoom class.
-    HotelRoom.Rooms();
+    
+                        HotelRoom.Rooms();
 
     }
 
