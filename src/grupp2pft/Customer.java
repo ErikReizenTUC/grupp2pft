625ac1e6efebb20ef00885d23dd22dfdd50c5aa8
Erik Reizenstein <erik.reizenstein.mte21lin@tucsweden.se>
2021-03-05 10:24:48 +0100

    Defined attributes and constructor for Customer class

--- src/grupp2pft/Customer.java
+++ src/grupp2pft/Customer.java
@@ -10,39 +10,25 @@ package grupp2pft;
  * @author Group 2
  */
 public class Customer {
-    //variable to hold the sum of all the services used by customer
-    //might be outside scope?
-    public int totalCost = 0;
+    //attributes for customers
+    public String firstName;
+    public String lastName;
     
-    //see all available rooms
-    public void SeeRoomOptions() {
-        /* will probably have to be connected to the switch case?
-        Something like:
-        for (int i = 0; i < roomList.size(); i++) {
-            System.out.println((i + 1) + ". " + roomList.get(i).roominfo);
-        }
-        input = scan.nextInt();
-        roomList.get(input).CheckIn();
-        */    
+    //constructor
+    public void Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
     
+    
     // checking in, getting the option of paying in advance or not.
-    public void CheckIn() {
+    public void CheckIn(HotelRoom room) {
         
     }
     
-    //pay in advance for the room - might just have this in CheckIn() method
-    public void PayInAdvance() {
-        //this might be redundant unless the customer has a certain amount of money to spend?
-    }
     
     //checking out, selecting various extra options
     public void CheckOut() {
-        /* will have to be connected to the switch case
-        totalCost += room.price;
-        totalCost += spa.price;
-        totalCost += resturant.price;
-        totalCost += minibar.price;
-        */
+
     }
 }
