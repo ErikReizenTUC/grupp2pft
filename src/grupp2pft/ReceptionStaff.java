05cb0559f1d9d0f1d30a5fb7d32cbb698e941639
Ali Esmaeeli <ali.esmaeeli.mte21lin@tucsweden.se>
2021-03-07 16:56:32 +0100

    Receptionist class

--- src/grupp2pft/ReceptionStaff.java
+++ src/grupp2pft/ReceptionStaff.java
@@ -5,6 +5,9 @@
  */
 package grupp2pft;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author stefa
@@ -15,4 +18,47 @@ public class ReceptionStaff {
     //Denna klass är just nu överflödig, vi avvaktar med kod här och skriver 
     // i HotelStaff-classen istället!
     
+    Scanner RecInput = new Scanner (System.in);
+    
+    //attributes for the receptionist
+    public String firstName;
+    public String lastName;
+    public int iD;
+    
+    //contructor
+    public void Receptionist (String firstName, String lastName, int iD) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.iD = iD;
+    }
+    //an arraylist to save the data for receptionist users
+    static ArrayList<ReceptionStaff> recStaff = new ArrayList<>();
+    
+        
+    public void AddRoom (HotelRoom room){
+        //ask the receptionist to verify its user ID for further access
+        System.out.println("Please enter your work ID: ");
+        int recID = RecInput.nextInt();
+        
+        // a conditional sentence to give access to the right staff
+        if (recID < 10 && recID != 0 ){
+            System.out.println("Access Authorized");
+            System.out.println("-------------------------------");
+            System.out.println("Choose one of the following: \n");
+            System.out.println("--------------------------------");
+            System.out.println("[a] Check for available rooms");
+            System.out.println("[b] Create a new available room");
+            System.out.println("[c] Mark a room unavailable");
+            System.out.println("[d] Delete a room");
+            System.out.println("[e] Find customer info");
+            System.out.println("[f] Checkout bill");
+        
+        
+        
+        } else {
+            System.out.println("Access Denied");
+            
+        }
+        
+    }
 }
