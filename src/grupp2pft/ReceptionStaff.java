c7894cad0f972f9533f5ec9bd40da141f1b99373
Stefan <stefan.levstad.mte21lin@tucsweden.se>
2021-04-07 10:44:24 +0200

    working on the new login method

--- src/grupp2pft/ReceptionStaff.java
+++ src/grupp2pft/ReceptionStaff.java
@@ -430,57 +430,66 @@ public class ReceptionStaff  {
     }
     
     //Log in as receptionist with password
-    public static boolean ReceptionistLogIn(){
-        
-        try {
-            //asking the receptionist to enter first name 
-            System.out.print("First Name: ");
-            //checking that input is only letters, english only
-            while (!RecInput.hasNext("[A-Za-z]+")) {
-                System.out.println("Invalid input!");
-                System.out.print("First Name: ");
-                RecInput.next();
-            }
-            String recFName = RecInput.next();
-            
-            //user inputs last name
-            System.out.print("Last Name: ");
-            //chgecking that input is only letters, english only
-            while (!RecInput.hasNext("[A-Za-z]+")) {
-                System.out.println("Invalid input!");
-                System.out.print("Last Name: ");
-                RecInput.next();
-            }
-            //asking user to input work id, has to be between 1 and 9 to be authorized
-            String recLName = RecInput.next();
-            System.out.print("Work ID: ");
-            int recId = RecInput.nextInt();
+    public static boolean ReceptionistLogIn() {
 
-            //saving the users input, creating new receptionstaff object
-            ReceptionStaff receptionist = new ReceptionStaff(recFName, recLName, recId);
-                     
+        boolean loginSuccess = false;
+
+        //for-loop with counter to check how many times login has been done.
+        for (int i = 0; i < 3; i++ ) {
+            try {
+                //asking the receptionist to enter first name 
+                System.out.print("First Name: ");
+                //checking that input is only letters, english only
+                while (!RecInput.hasNext("[A-Za-z]+")) {
+                    System.out.println("Invalid input!");
+                    System.out.print("First Name: ");
+                    RecInput.next();
+                }
+                String recFName = RecInput.next();
+
+                //user inputs last name
+                System.out.print("Last Name: ");
+                //chgecking that input is only letters, english only
+                while (!RecInput.hasNext("[A-Za-z]+")) {
+                    System.out.println("Invalid input!");
+                    System.out.print("Last Name: ");
+                    RecInput.next();
+                }
+                //asking user to input work id, has to be between 1 and 9 to be authorized
+                String recLName = RecInput.next();
+                System.out.print("Work ID: ");
+                int recId = RecInput.nextInt();
+
+                //saving the users input, creating new receptionstaff object
+                ReceptionStaff receptionist = new ReceptionStaff(recFName, recLName, recId);
+
                 //if statement for either accessing  the main menu or being redirected to the user 
                 //selection menu
-                if (recId > 0 && recId < 10){
-                    
+                if (recId > 0 && recId < 10) {
+
                     System.out.println("-----------------------------");
                     System.out.println("Access Authorized");
                     System.out.println("-----------------------------");
 
                     System.out.println("Welcome " + receptionist.firstName + " " + receptionist.lastName);
                     System.out.println("-----------------------------");
+                    
+                    loginSuccess = true;
+                }
+            } catch (Exception InputMismatchException) {
+                //handling if input is not integer
+                System.out.println("Please enter a number");
+                System.out.println("-----------------------------");
 
-        }
+                //Cleaning scanner
+                RecInput.next();
             }
-                catch (Exception InputMismatchException) {
-            //handling if input is not integer
-            System.out.println("Please enter a number");
-            System.out.println("-----------------------------");
-            
-            //Cleaning scanner
-            RecInput.next();
         }
-        return false;
+        
+        if(loginSuccess == false){
+           return false; 
+        } else return true;
+        
     }
     
     //Creating a new receptionist object.
