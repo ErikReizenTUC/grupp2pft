e1b855a8f005e7fc039e43ff11f272bd9dbf8a3f
Stefan <stefan.levstad.mte21lin@tucsweden.se>
2021-04-15 13:32:20 +0200

    working on the new count method

--- src/grupp2pft/RoomService.java
+++ src/grupp2pft/RoomService.java
@@ -1,5 +1,7 @@
 package grupp2pft;
 
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -26,5 +28,20 @@ public class RoomService {
         return this.productPrice * number;
     }
     
+    //ArrayList with productObjects.
+    static ArrayList<RoomService> productList = new ArrayList<>();
     
+    //New method to display available products
+    public static int DisplayProducts(){
+        
+        int count = 0;
+        
+        //printing All products
+            for (int i = 0; i < RoomService.productList.size(); i++){
+                System.out.println(RoomService.productList.get(i).productName + ", " +
+                        RoomService.productList.get(i).productPrice + " kr.");
+                count++;
+            }
+        return count;
+    }
 }
